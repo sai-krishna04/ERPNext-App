f2bcb9a6c21e91ffcacd24582c17467df285381c
sai-krishna04 <apsaikrishna4@gmail.com>
2026-08-14 13:05:25 +0530

    Installed bank api

--- erpnext_app/api.py
+++ erpnext_app/api.py
@@ -1,19 +1,28 @@
-
-
 import frappe
 import requests
-import uuid
-@frappe.whitelist()
-def verify_payment_otp(phone, otp,purchase_invoice=None,
-    bank_account=None,
-    amount=None,reciever=None):
 
-    url = "http://integ.local:8000/api/method/integ_app.api.validate_otp"
+
+@frappe.whitelist()
+def verify_payment_otp(
+    phone,
+    otp,
+    purchase_invoice=None,
+    bank_account=None,
+    receiver=None,
+    amount=None,
+    payment_type=None,
+    unique_id=None
+):
+
+    url = (
+        "http://integ.local:8000/"
+        "api/method/integ_app.api.validate_otp"
+    )
 
     headers = {
         "Authorization": "token cbdf2692bd670b0:3752219d3d08b14"
     }
-
+    frappe.msgprint(receiver)
     response = requests.post(
         url,
         headers=headers,
@@ -22,8 +31,10 @@ def verify_payment_otp(phone, otp,purchase_invoice=None,
             "otp": otp,
             "purchase_invoice": purchase_invoice,
             "bank_account": bank_account,
+            "receiver_no": receiver,
             "amount": amount,
-            "reciever":reciever
+            "payment_type": payment_type,
+            "unique_id":unique_id
         },
         timeout=10
     )
@@ -37,4 +48,3 @@ def verify_payment_otp(phone, otp,purchase_invoice=None,
     result = response.json()
 
     return result.get("message")
-
